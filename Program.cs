5f92bbdcba9dc5d1010a35ccde19a6acbf6196b1
Gregory Grant <grant8827@yahoo.com>
2026-02-19 12:01:36 -0500

    Allow rightfitgigs.com frontend origin in CORS

--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ var allowedOrigins = new[]
 {
     frontendUrl,
     defaultFrontendUrl,
+    "https://rightfitgigs.com",
+    "https://www.rightfitgigs.com",
     "http://localhost:5173",
     "http://127.0.0.1:5173"
 }
